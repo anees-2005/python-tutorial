9bb859b07b9a02f0504e2152292e824da70da7b6
anees <anees200511@gmail.com>
2025-10-24 15:41:01 +0530

    updated commit

--- basics.py
+++ basics.py
@@ -29,3 +29,10 @@
 # print(a<=b)
 
 
+#logical op
+a=5
+b=6
+print(a> 3 and b < 10)# double value true and print `true` but values true and false output as a false
+print(a> 3 or b <1)# and model but vluebel `true` 
+print(not(a>3))# this code value is `true` but  out put displayed false
+
